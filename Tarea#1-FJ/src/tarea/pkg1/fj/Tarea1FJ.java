c2775ef8708bc70030210f9cb195642939c0ac49
ferdmeri <ferdmeri@espol.edu.ec>
2015-11-08 21:16:03 -0500

    Update Tarea1FJ.java

--- Tarea#1-FJ/src/tarea/pkg1/fj/Tarea1FJ.java
+++ Tarea#1-FJ/src/tarea/pkg1/fj/Tarea1FJ.java
@@ -18,6 +18,7 @@ public class Tarea1FJ {
         System.out.println("               HOLA MUNDO");
         System.out.println("            FERNANDO MERINO");
         System.out.println("            FERNANDO ZATIZABAL");
+        System.out.println("              JULIO AREVALO");
         System.out.println("              BARCELONA SC");
         System.out.println("      FUNDAMENTOS DE JAVA - ESPOL");
          System.out.println("              LICRED-FIEC");
